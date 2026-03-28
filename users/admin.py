ddbc58fab60a2a6fcd539d0821294997c0d31bba
331999 <3319958124@qq.com>
2026-03-28 13:29:28 +0800

    feat(users): 扩展 User 模型添加昵称、头像、简介、安全问题字段

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """用户管理"""
+    list_display = ['username', 'nickname', 'email', 'is_active', 'is_staff']
+    list_filter = ['is_active', 'is_staff']
+    search_fields = ['username', 'nickname', 'email']
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('扩展信息', {
+            'fields': ('nickname', 'avatar', 'bio', 'security_question', 'security_answer')
+        }),
+    )
+
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('扩展信息', {
+            'fields': ('nickname', 'avatar', 'bio', 'security_question', 'security_answer')
+        }),
+    )
--- users/models.py
+++ users/models.py
@@ -2,11 +2,36 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+# 安全问题选项
+SECURITY_QUESTIONS = [
+    ('mother_name', '您母亲的姓名是？'),
+    ('birth_city', '您的出生城市是？'),
+    ('first_school', '您的第一所学校名称是？'),
+    ('favorite_book', '您最喜欢的书籍是？'),
+]
+
+
 class User(AbstractUser):
-    # 定义用户认证模型类
-    pass
+    """用户模型"""
+    nickname = models.CharField('昵称', max_length=50, blank=True, default='')
+    avatar = models.ImageField('头像', upload_to='avatars/', blank=True, null=True)
+    bio = models.TextField('个人简介', max_length=500, blank=True, default='')
+    security_question = models.CharField(
+        '安全问题',
+        max_length=20,
+        blank=True,
+        choices=SECURITY_QUESTIONS
+    )
+    security_answer = models.CharField('安全问题答案', max_length=100, blank=True, default='')
 
     class Meta:
         db_table = 'user'
         verbose_name = '用户'
         verbose_name_plural = '用户'
+
+    def __str__(self):
+        return self.nickname or self.username
+
+    def get_display_name(self):
+        """获取显示名称：优先昵称，其次学号"""
+        return self.nickname if self.nickname else self.username

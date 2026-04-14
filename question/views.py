4577cba0d9d0ffc277dc5378968664dfec0e914d
331999 <155950552+331999@users.noreply.github.com>
2026-04-14 12:17:08 +0800

    Clean up views.py by removing unused code
    Removed commented-out code related to OpenAI integration and chat functionality.

--- question/views.py
+++ question/views.py
@@ -3,48 +3,6 @@ from django.views import View
 from django.http import JsonResponse, HttpResponseNotFound
 from .bot.send_question import bot
 import os
-# from openai import OpenAI
-
-#
-# class Question(View):
-#     def get(self, request):
-#         return render(request, 'question.html')
-#
-#     def post(self, request):
-#         client = OpenAI(
-#             api_key="sk-8CA9vewKMVCLDNQvKeY10ZcI1cOhsza35KWKyAn9jujPIXOL",
-#             base_url="https://api.moonshot.cn/v1",
-#         )
-#
-#         history = [
-#             {"role": "system",
-#              "content": "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。"}
-#         ]
-#
-#         def chat(query, history):
-#             history.append({
-#                 "role": "user",
-#                 "content": query
-#             })
-#             completion = client.chat.completions.create(
-#                 model="moonshot-v1-8k",
-#                 messages=history,
-#                 temperature=0.3,
-#             )
-#             result = completion.choices[0].message.content
-#             history.append({
-#                 "role": "assistant",
-#                 "content": result
-#             })
-#             return result
-#
-#             while True:
-#                 query = request.POST.get('question')
-#                 if query.lower() == "退出":
-#                     break
-#                 answer = chat(query, history)
-#                 return JsonResponse({'code': 200, 'errmsg': 'OK', 'answer': answer})
-
 
 class Question(View):
     def get(self, request):
